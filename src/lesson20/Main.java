fbb43dd0d4448ac1107fa979cf40bfeb51396689
Azat90210 <azat_market@mail.ru>
2022-10-12 21:32:56 +0300

    Lesson20

--- src/lesson20/Main.java
+++ src/lesson20/Main.java
@@ -7,6 +7,12 @@ public class Main {
         cIO.copy("C:\\Users\\user\\Desktop\\ДБО\\ФОТО 1 смена 2020\\Солнечный\\_jlGPwO1eEQ.jpg", "qwe.jpg");
         long end = System.currentTimeMillis();
         System.out.println("Время работы старого IO = " + (end - start));
-
+        System.out.println("_________________________________");
+        CopyNIOFile copyNIOFile = new CopyNIOFile();
+        long start1 = System.currentTimeMillis();
+        copyNIOFile.copy("C:\\Users\\user\\Desktop\\ДБО\\ФОТО 1 смена 2020\\Солнечный\\_jlGPwO1eEQ.jpg", "qwe1.jpg");
+        cIO.copy("C:\\Users\\user\\Desktop\\ДБО\\ФОТО 1 смена 2020\\Солнечный\\_jlGPwO1eEQ.jpg", "qwe.jpg");
+        long end1 = System.currentTimeMillis();
+        System.out.println("Время работы нового IO = " + (end1 - start1));
     }
 }
