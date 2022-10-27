459fe7f78a1b1a015ef00cc187295bf43cb2faa2
Azat90210 <azat_market@mail.ru>
2022-10-27 22:05:28 +0300

    Pro pevcov bez log4j

--- src/lesson22/Pevec.java
+++ src/lesson22/Pevec.java
@@ -4,11 +4,11 @@ public class Pevec extends Thread {
     @Override
     public void run() {
         int count = 0;
-        while (count<3) {
+        while (count<9) {
             for (int i = 0; i < 3; i++) {
                 System.out.println("La - la - la");
                 try {
-                    Thread.sleep(500);
+                    Thread.sleep(300);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
--- src/lesson22/Veduschi.java
+++ src/lesson22/Veduschi.java
@@ -5,9 +5,16 @@ public class Veduschi {
         System.out.println("Начало концерта");
         Pevec pevec = new Pevec();
         pevec.start();
-        Shutnik shutnik = new Shutnik();
-        shutnik.setDaemon(true);
-        shutnik.start();
+//        Shutnik shutnik = new Shutnik();
+//        shutnik.setDaemon(true);
+//        shutnik.start();
+        Pevec2 pevec2 = new Pevec2();
+        pevec2.setDaemon(true);
+        pevec2.start();
+
+        Pevec3 pevec3 = new Pevec3();
+        pevec3.setDaemon(true);
+        pevec3.start();
         while (pevec.isAlive()) {
         }
         System.out.println("Концерт закончился! Расходитесь");
